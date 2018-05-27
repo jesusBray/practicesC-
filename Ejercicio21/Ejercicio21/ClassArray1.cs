4ba59e64ca8b4988982811dfc9133b873b32f17a
JesusBryanPaye <jesus.brayan.paye@gmail.com>
2018-05-26 22:53:01 -0400

    "Ejercicio con arrrays"

---
+++ Ejercicio21/Ejercicio21/ClassArray1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio21
+{
+    class ClassArray1
+    {
+        int[] vec;
+
+        public ClassArray1(int[] vec)
+        {
+            this.vec = vec;
+        }
+
+        public ClassArray1(){}
+
+        public int SumaArreglo()
+        {
+            int suma=0;
+            for (int i = 0; i < vec.Length; i++)
+            {
+                suma = suma + vec[i];
+            }
+            return suma;
+        }
+
+        public int MayorQue()
+        {
+            int aux = vec[0];
+            for (int i = 1; i < vec.Length; i++)
+            {
+                 if (aux<vec[i])
+                {
+                    aux = vec[i];
+                }
+            }
+            return aux;
+        }
+
+        public int MenorQue()
+        {
+            int aux = vec[0];
+            for (int i = 1; i < vec.Length; i++)
+            {
+                if (aux > vec[i])
+                {
+                    aux = vec[i];
+                }
+            }
+            return aux;
+        }
+
+        public double MediaElementos()
+        {
+            int media = vec[0];
+            for (int i = 0; i < vec.Length; i++)
+            {
+                if (media<vec[i])
+                {
+                    media = i;
+                }
+            }
+            
+            return SumaArreglo()/vec.Length;
+        }
+
+    }
+}
---
+++ Ejercicio21/Ejercicio21/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+namespace Ejercicio21
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int[] array;
+        ClassArray1 vect;
+
+        private void Inicial(object sender, RoutedEventArgs e)
+        {
+            Random aleatorio = new Random();
+            int numeroElementos = int.Parse(Interaction.InputBox("Ingrese el numero de indices que tendra el array", "Array Aplication"));
+
+            array = new int[numeroElementos];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = aleatorio.Next(1,100);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                lbxLista.Items.Add(array[i]);
+            }
+
+        }
+        private void ClicSumaElementos(object sender, RoutedEventArgs e)
+        {
+            vect = new ClassArray1(array);
+            lbxLista.Items.Add(vect.SumaArreglo());
+        }
+
+        private void ClicMayorElemento(object sender, RoutedEventArgs e)
+        {
+            vect = new ClassArray1(array);
+            lbxLista.Items.Add(vect.MayorQue());
+        }
+
+        private void ClicMenorElemento(object sender, RoutedEventArgs e)
+        {
+            vect = new ClassArray1(array);
+            lbxLista.Items.Add(vect.MenorQue());
+        }
+
+        private void ClicMediaElementos(object sender, RoutedEventArgs e)
+        {
+            ClassArray1 vect = new ClassArray1(array);
+            lbxLista.Items.Add(vect.MediaElementos());
+        }
+
+        private void clicBorrar(object sender, RoutedEventArgs e)
+        {
+            lbxLista.Items.Clear();
+        }
+    }
+}
