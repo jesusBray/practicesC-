7d3bbbd66fbfaf3196e5238bdf34b87f6fbfa2b2
jesusBray <jesus.brayan.paye@gmail.com>
2018-07-02 19:24:53 -0400

    exaple verific please

---
+++ Practica69/Practica69/Cadenas.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica69
+{
+    class Cadenas
+    {
+        public string palindromo(string dato)
+        {
+            string mensaje = "";
+            for (int i = dato.Length; i > 1; i--)
+            {
+                mensaje = mensaje + dato[i-1];
+                if (dato.Equals(mensaje))
+                {
+                    return "Es un palindromo";
+                }   
+            }
+            return "No es un palindromo";
+        }
+
+        public int CantidadPalabras(string dato)
+        {
+            char espacio = ' ';
+            int contador = 0;
+            for (int i = 0; i < dato.Length; i++)
+            {
+                if (dato[i].Equals(espacio))
+                {
+                    contador ++;
+                }
+            }
+            return (contador+1);
+        }
+        public bool BuscarCaracter(char caracter,string cadena)
+        {
+            bool resultado= false;
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena[i]==caracter)
+                {
+                    resultado = true;
+                    break;
+                }
+            }
+            return resultado;
+        }
+        public bool CaracterNumber(char c,string dato)
+        {
+            bool resultado = false;
+            for (int i = 0; i < dato.Length; i++)
+            {
+                if (char.IsNumber(dato[i])==true)
+                {
+                resultado = true;
+                break;
+                }
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Practica69/Practica69/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+
+namespace Practica69
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Cadenas cadena ;
+        private void ClicPalindromo(object sender, RoutedEventArgs e)
+        {
+            cadena = new Cadenas();
+            lbxLista.Items.Add(cadena.palindromo(txtDato.Text));
+        }
+
+        private void clicPalabras(object sender, RoutedEventArgs e)
+        {
+            lbxLista.Items.Clear();
+            cadena = new Cadenas();
+            lbxLista.Items.Add(cadena.CantidadPalabras(txtDato.Text));
+        }
+
+        private void clicBuscarCaracter(object sender, RoutedEventArgs e)
+        {
+            cadena = new Cadenas();
+            char dato = char.Parse(Interaction.InputBox("Digite su caracter ", "Caracteres"));
+            if (cadena.BuscarCaracter(dato, txtDato.Text)==true)
+            {
+                lbxLista.Items.Add("El caracter es existente");
+            }
+            else
+            {
+                lbxLista.Items.Add("El caracter no es existente");
+            }
+        }
+
+        private void ClickLimpiarDAtos(object sender, RoutedEventArgs e)
+        {
+            txtDato.Text = "";
+            lbxLista.Items.Clear();
+        }
+
+
+        private void clicNew(object sender, RoutedEventArgs e)
+        {
+            new Win().Show();
+        }
+
+        private void ClicBuscarNumeros(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            {
+                
+            }
+        }
+        
+    }
+}
