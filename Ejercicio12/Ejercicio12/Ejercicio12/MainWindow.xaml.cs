f6b9765008dcdc2a9bb2dd58294b742db644638a
jesusBray <jesus.brayan.paye@gmail.com>
2018-07-02 19:33:24 -0400

    more examples made in the classroom

---
+++ Ejercicio12/Ejercicio12/Ejercicio12/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int num1,num2,num3, result;
+        Random aleatorio = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClicResultado2(object sender, RoutedEventArgs e)
+        {
+            num1 = aleatorio.Next(1, 7);
+            num2 = aleatorio.Next(1, 7);
+            lblDado1.Content = num1;
+            lblDado1.Content = num2;
+            if(num1>(num2-1)&&num2>(num1-1)){
+                lblResultado1.Content = "Ganaste Salio escalera";
+            }
+            else
+            {
+                lblResultado1.Content = "Perdedor no Salio escalera";
+            }
+        }
+
+        private void clicResultado1(object sender, ContextMenuEventArgs e)
+        {
+            num1 = aleatorio.Next(1,7);
+            num2 = aleatorio.Next(1,7);
+            num3 = aleatorio.Next(1,7);
+            lblResp1.Content = num1;
+            lblResp2.Content = num2;
+            lblResp3.Content = num3;
+            if (num1 > (num2 - 1)&&num1>(num3-1)||num2>(num1-1)&&num2>(num3-1)||num3>(num1-1)&&num3>(num2-1))
+            {
+                lblResultado2.Content = "Ganaste Sacaste Escalera de 3 dados";
+            }
+            else
+            {
+                lblResultado2.Content = "Perdedor no Sacaste Escalera de 3 dados";
+            }
+        }
+    }
+}
---
+++ Ejercicio12/Ejercicio12/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio12
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double num1, num2,resul;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clicResultado(object sender, RoutedEventArgs e)
+        {
+            char letra1;
+           
+            letra1 = char.Parse(txtDato1.Text);
+
+            if (letra1 == 'a' || letra1 == 'e' || letra1 == 'i' || letra1 == 'o' || letra1 == 'u')
+            {
+                MessageBox.Show("El caracter es una bocal");
+            }
+            else
+            {
+                MessageBox.Show("El caracter no es una bocal");
+            }
+            
+        }
+
+        private void clicresutlado2(object sender, RoutedEventArgs e)
+        {
+
+            num1 = double.Parse(txtDatoNumero.Text);
+            num2 = double.Parse(txtDatoNumero2.Text);
+            if(num1> num2){
+                MessageBox.Show("Resultado "+num1+": "+num2);
+            }
+            else
+            {
+                MessageBox.Show("Resultado " + num2 + ": " + num1);
+            }
+        }
+
+        private void clicNumero3(object sender, RoutedEventArgs e)
+        {
+            Random aleatorio = new Random();
+            double num1 = aleatorio.Next(1, 7);
+            double num2 = aleatorio.Next(1, 7);
+            lblDado1.Content = num1;
+            lblDado2.Content = num2;
+            if(num1 == (num2-1) || num2 == (num1-1)){
+                lblResul.Content=("El nuemero es una escalera "+num1+" : "+num2);
+            }
+            else
+            {
+                lblResul.Content = ("El nuemero no es una escalera " + num1 + " : " + num2);
+            }
+        }
+    }
+}
